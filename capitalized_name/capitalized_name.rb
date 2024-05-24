b44d96666c883a49daaf9b9d037899a796b7431c
Gudiao21 <lucianodantas53@gmail.com>
2024-05-24 15:29:34 -0300

    Add .vscode/launch.json

---
+++ capitalized_name/capitalized_name.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require 'pry-nav'
+
+a = ["ana maria", "joão silva"]
+name_composto = a.map do |name|
+  partes = name.split(' ')
+  first_name = partes[0].capitalize
+  last_name = partes[-1].capitalize
+  "#{first_name} #{last_name}"
+  #binding.pry
+end
+puts name_composto
